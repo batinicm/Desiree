d2e23d7d99923a22c1d875cfaf525eb9c7db2383
Milica Batinic <milica.batinic12@gmail.com>
2023-07-01 21:40:06 +0200

    Changes.

--- Analyzer/analyzer.py
+++ Analyzer/analyzer.py
@@ -1,4 +1,14 @@
 # Analyze initial corpus of song lyrics to have for app startup
 # 1. Get song lyrics for current top 40, 80s, 90s, 00s and 10s classics
 # 2. Analyze song lyrics and assign sentiment to them
+import os
+from azure.core.credentials import AzureKeyCredential
+from azure.ai.textanalytics import TextAnalyticsClient
+import vault_worker
 
+
+def analyze_text(text):
+    endpoint = vault_worker.get_secret("LanguageAnalyzerEndpoint")
+    key = vault_worker.get_secret("LanguageAnalyzerKey")
+
+    text_analytics_client = TextAnalyticsClient(endpoint, AzureKeyCredential(key))
