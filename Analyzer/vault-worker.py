14826509bbb26a89b82c92facc17b30d864b4495
Milica Batinic <milica.batinic12@gmail.com>
2023-07-01 20:54:51 +0200

    Added vault secret retrieval.

--- Analyzer/vault-worker.py
+++ Analyzer/vault-worker.py
@@ -4,15 +4,15 @@ import os
 from azure.keyvault.secrets import SecretClient
 from azure.identity import DefaultAzureCredential
 
-key_vault_name = os.environ["KEY_VAULT_NAME"]
+key_vault_name = "desiree-vault"
 kv_uri = f"https://{key_vault_name}.vault.azure.net"
 
 credential = DefaultAzureCredential()
 client = SecretClient(vault_url=kv_uri, credential=credential)
 
 
-def get_vault_secret(secret_name):
+if __name__ == '__main__':
     print(f"Retrieving your secret from {key_vault_name}.")
-    secret = client.get_secret(secret_name)
+    secret = client.get_secret("LanguageAnalyzerEndpoint")
 
-    return secret
+    print(secret)
