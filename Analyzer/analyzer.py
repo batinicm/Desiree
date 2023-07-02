c36763f4dd1de949d84c2b234e9472235532db1b
Milica Batinic <milica.batinic12@gmail.com>
2023-07-02 19:23:55 +0200

    Added track fetching from spotify playlist.

--- Analyzer/analyzer.py
+++ Analyzer/analyzer.py
@@ -5,9 +5,11 @@ import os
 from azure.core.credentials import AzureKeyCredential
 from azure.ai.textanalytics import TextAnalyticsClient
 import vault_worker
+from spotipy.oauth2 import SpotifyClientCredentials
+import spotipy
 
-
-# if __name__ == '__main__':
+ALL_OUT_PLAYLIST_IDS = ['37i9dQZF1DX5Ejj0EkURtP', '37i9dQZF1DX4o1oenSJRJd', '37i9dQZF1DXbTxeAdrVG2l',
+                        '37i9dQZF1DX4UtSsGT1Sbe']
 
 
 # TODO: Use Spotify API to get playlist contents (aka songs in playlists)
@@ -17,8 +19,47 @@ import vault_worker
 # on retrieved lyrics
 # ? Store result in Azure storage account for that
 
+def extract_track_info(track):
+    return {
+        "Name": track['track']['name'],
+        "Artists": [artist['name'] for artist in track['track']['artists']]
+    }
+
+
+# Get playlist contents
+# Information to keep: track name, track artist
+def get_tracks():
+    sp = spotipy.Spotify(client_credentials_manager=SpotifyClientCredentials())
+    all_tracks = []
+
+    for playlist_id in ALL_OUT_PLAYLIST_IDS:
+        pl_id = 'spotify:playlist:' + playlist_id
+        offset = 0
+        while True:
+            response = sp.playlist_items(pl_id,
+                                         offset=offset,
+                                         fields='items',
+                                         additional_types=['track'])
+            if len(response['items']) == 0:
+                break
+
+            items = list(map(extract_track_info, response['items']))
+            print(items)
+            offset = offset + len(items)
+            all_tracks.append(items)
+
+    return all_tracks
+
+
+def get_lyrics():
+
+
 def analyze_text(text):
     endpoint = vault_worker.get_secret("LanguageAnalyzerEndpoint")
     key = vault_worker.get_secret("LanguageAnalyzerKey")
 
     text_analytics_client = TextAnalyticsClient(endpoint, AzureKeyCredential(key))
+
+
+if __name__ == '__main__':
+    get_tracks()
