b02bdeef4756bb0ca7da91956cea4e5fa5e9a74f
Milica Batinic <milica.batinic12@gmail.com>
2023-07-16 14:07:10 +0200

    Changes.

--- Analyzer/Model/track_class.py
+++ Analyzer/Model/track_class.py
@@ -1,6 +1,14 @@
 class Track:
-    def __init__(self, spotify_id, name, artists, lyrics = None):
+    def __init__(self, spotify_id, name, artists, lyrics=None):
         self.spotify_id = spotify_id
         self.name = name
         self.artists = artists
         self.lyrics = lyrics
+
+    def to_dict(track):
+        return {
+            "SpotifyId": track.spotify_id,
+            "Name": track.name,
+            "Artists": track.artists,
+            "Lyrics": track.lyrics
+        }
--- Analyzer/analyzer.py
+++ Analyzer/analyzer.py
@@ -11,11 +11,16 @@ from lyricsgenius import Genius
 import re
 from azure.data.tables import TableServiceClient
 from Model.track_class import Track
+import pandas
+from azure.ai.textanalytics import TextAnalyticsClient
+from azure.core.credentials import AzureKeyCredential
 
 ALL_OUT_PLAYLIST_IDS = ['37i9dQZF1DX5Ejj0EkURtP', '37i9dQZF1DX4o1oenSJRJd', '37i9dQZF1DXbTxeAdrVG2l',
                         '37i9dQZF1DX4UtSsGT1Sbe']
-
 LYRICS_TABLE_NAME = 'Lyrics'
+BATCH_OPERATION_COUNT = 20
+LYRICS_TABLE_ENTITY_COLUMNS = ['Partition', 'RowKey', 'TrackName', 'Artists', 'Lyrics']
+
 
 # Use Spotify API to get playlist contents (aka songs in playlists)
 # Dedupe song names
@@ -26,31 +31,34 @@ LYRICS_TABLE_NAME = 'Lyrics'
 
 # Extract necessary information from the track information retrieved from Spotify
 def extract_track_info(track):
-    return Track(spotify_id=track['track']['id'], name=track['track']['name'], artists=[artist['name'] for artist in track['track']['artists']])
+    return Track(spotify_id=track['track']['id'], name=track['track']['name'],
+                 artists=[artist['name'] for artist in track['track']['artists']])
 
 
 # Get playlist contents
-def get_tracks():
+def get_tracks(playlist_id):
+    print("Starting get_tracks:")
+
     sp = spotipy.Spotify(client_credentials_manager=SpotifyClientCredentials())
     all_tracks = []
 
-    for playlist_id in ALL_OUT_PLAYLIST_IDS:
-        pl_id = 'spotify:playlist:' + playlist_id
-        offset = 0
-        while True:
-            response = sp.playlist_items(pl_id,
-                                         offset=offset,
-                                         fields='items',
-                                         additional_types=['track'])
-            if len(response['items']) == 0:
-                break
+    pl_id = 'spotify:playlist:' + playlist_id
+    offset = 0
+    while True:
+        response = sp.playlist_items(pl_id,
+                                     offset=offset,
+                                     fields='items',
+                                     additional_types=['track'])
+        if len(response['items']) == 0:
+            break
 
-            items = list(map(extract_track_info, response['items']))
-            print(items)
-            offset = offset + len(items)
-            all_tracks = all_tracks + items
+        items = list(map(extract_track_info, response['items']))
+        print(items)
+        offset = offset + len(items)
+        all_tracks = all_tracks + items
 
     print("Total song number: " + str(len(all_tracks)))
+    print("get_tracks done.")
     return all_tracks
 
 
@@ -61,29 +69,48 @@ def purge_feat_from_title(title):
 
 # Get lyrics to songs using Genius API
 def get_lyrics(track):
+    print("Fetching lyrics")
     genius_token = vault_worker.get_secret("GeniusClientAccessToken")
 
-    genius = Genius(genius_token)
+    genius = Genius(access_token=genius_token, sleep_time=1, retries=5)
     artist = genius.search_artist(next(iter(track.artists or [])), max_songs=0)
 
     song_title = purge_feat_from_title(track.name)
     song = genius.search_song(song_title, artist.name)
 
     if song is None:
-        raise Exception("Song not found")
+        track.lyrics = ""
+    else:
+        track.lyrics = song.lyrics
 
-    track.lyrics = song.lyrics
+    print("Lyrics fetched")
     return track
 
 
 # Preprocess lyrics to eliminate part of song annotations (verse, chorus, singer...)
 # and additional details at the beginning of the song
-def prepare_for_analysis(track):
-    lyrics = re.sub("\[.*\]", "", track.lyrics)
-    lyrics = lyrics.split("\n")
-    track.lyrics = "\n".join(lyrics[1:- 1])
+def process_lyrics_for_analysis(lyrics):
+    cleaned_lyrics = re.sub("\[.*\]", "", lyrics)
+    cleaned_lyrics = cleaned_lyrics.split("\n")
+    return "\n".join(cleaned_lyrics[1:- 1])
 
-    return track
+
+# Remove duplicate songs (recognized by the same spotify guid)
+# Remove rows which have empty 'Lyrics' column
+# Prepare lyrics for analysis
+def prepare_for_analysis(tracks):
+    dataframe = pandas.DataFrame(map(Track.to_dict, tracks))
+    print(dataframe)
+
+    dataframe.drop_duplicates(subset=['SpotifyId'], inplace=True)
+    print(dataframe)
+
+    dataframe['Lyrics'].str.strip()
+    dataframe.drop(dataframe[dataframe['Lyrics'] == ""].index, inplace=True)
+    print(dataframe)
+
+    dataframe['Lyrics'] = list(map(process_lyrics_for_analysis, dataframe['Lyrics']))
+    return dataframe
 
 
 # Check if any track has been written to the storage: if yes, no additional querying of Spotify/Genius is needed
@@ -96,27 +123,55 @@ def check_storage_created():
     return len(list(entities)) != 0
 
 
+def delete_entities():
+    connection_string = vault_worker.get_secret("StorageAccountConnectionString")
+    table_service_client = TableServiceClient.from_connection_string(conn_str=connection_string)
+    table_client = table_service_client.get_table_client(table_name=LYRICS_TABLE_NAME)
+    entities = table_client.query_entities(query_filter="PartitionKey ne ''")
+
+    for entity in entities:
+        table_client.delete_entity(entity["PartitionKey"], entity["RowKey"])
+
+
+def get_partition_key(artists):
+    artist = next(iter(artists or []))
+    artist = artist.replace('\\', '')
+    artist = artist.replace('/', '')
+    return artist
+
+
 # Partition key: first artist name
 # Row key: song spotify id - from the class itself (id.name? for easier search?)
-def store_single_track(track, table_client):
-    table_entity = {
-        u'PartitionKey': next(iter(track.artists or [])),
-        u'RowKey': track.spotify_id,
-        u'TrackName': track.name,
-        u'Artists': ",".join(track.artists),
-        u'Lyrics': track.lyrics
-    }
-    table_client.create_entity(entity=table_entity)
+def create_lyric_table_operation(track_row):
+    track_info = track_row[1]
+    return ('upsert', {
+        u'PartitionKey': get_partition_key(track_info['Artists']),
+        u'RowKey': track_info['SpotifyId'],
+        u'Name': track_info['Name'],
+        u'Artists': ",".join(track_info['Artists']),
+        u'Lyrics': track_info['Lyrics']
+    })
 
 
 # Store song with lyrics in Azure Storage Account
-def store_lyrics(lyrics):
+def store_lyrics(tracks):
+    print("Store lyrics started")
+
     connection_string = vault_worker.get_secret("StorageAccountConnectionString")
     table_service_client = TableServiceClient.from_connection_string(conn_str=connection_string)
     table_client = table_service_client.get_table_client(table_name=LYRICS_TABLE_NAME)
 
-    for lyric in lyrics:
-        store_single_track(lyric, table_client)
+    operations = pandas.DataFrame()
+    operations['PartitionKey'] = list(map(get_partition_key, tracks['Artists']))
+    operations['Operation'] = list(map(create_lyric_table_operation, tracks.iterrows()))
+
+    # After groupby - list of table_entity for each partition key
+    grouped = operations.groupby('PartitionKey').agg(list)
+
+    for _, group in grouped.iterrows():
+        table_client.submit_transaction(group['Operation'])
+
+    print("Lyrics stored")
 
 
 def analyze_text(text):
@@ -127,9 +182,11 @@ def analyze_text(text):
 
 
 if __name__ == '__main__':
+    # delete_entities()
 
     if not check_storage_created():
-        tracks = get_tracks()
-        lyrics = map(get_lyrics, tracks)
-        lyrics = map(prepare_for_analysis, lyrics)
-        store_lyrics(lyrics)
+        for playlist in ALL_OUT_PLAYLIST_IDS:
+            tracks = get_tracks(playlist)
+            lyrics = map(get_lyrics, tracks)
+            lyrics = prepare_for_analysis(lyrics)
+            store_lyrics(lyrics)
